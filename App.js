62dfcefc7a22d598b2d544a4afe62050578cecfb
Matheus C.R <dev@mcarvalhor.com>
2023-02-15 17:13:46 -0300

    Added verification whether file exists or not.

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 const { fileURLToPath } = require('url');
 const app = express()
-const port = 3000
-var fs = require('fs');
+
+const port = 3000;
+const password = 'coelho';
+const file_path = './public/{name}.mp3';
 
 
+app.get('/requestaudio/:name', (req, res) => {
 
-app.get('/requestaudio/:pass/:name', (req, res) => {
-    console.log(req.params['pass']);
-    console.log(req.params['name']);
-  if(req.params['pass'] == 'coelho'){
-    
-   // try{
-      var fileRequest = new File ('public/'+req.params['name']+'.mp3');
-   // }catch{}
-
-    if(fileRequest.exists()){
-      res.sendFile(fileRequest);
-    }
-    else{
-        res.sendStatus(404);
-    }
+  if(req.query.pass !== password) { // Senha incorreta.
+    res.status(401).json({'error': 'Not authorized! Check if the password is correct.'});
+    return;
   }
-  else{
-    res.sendStatus(401);
-}
+
+  let name = req.params['name'].toLowerCase();
+  let audio_filename = file_path.replace('{name}', encodeURIComponent(name));
+
+  if(!fs.existsSync(audio_filename)) { // Arquivo não existe, temos que baixar ele da AWS.
+    res.status(404).json({'error': 'Not Found! This audio does not exist, and we could not download it from AWS at this time...'}); // TODO: baixar da AWS (lembrete: checar limites diários/mensais antes de baixar).
+    return;
+  }
+
+  //Enviar arquivo, que agora já existe.
+  res.download(audio_filename);
+
 })
+
+
+app.get('/', (req, res) => {
+  res.status(400).json({'error': 'Bad request! Use this format for the URL instead: '+ req.protocol +'://'+ req.headers.host +'/requestaudio/NAME?pass=PASSWORD'});
+  // Alternativamente, se você não quiser deixar claro como usa a API, descomente o de baixo e comente o de cima.
+  //res.status(400).json({'error': 'Bad request!'});
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
